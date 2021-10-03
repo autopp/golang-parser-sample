ea031747f3a3e3d1e9dfcfd1deede3a1ce95d780
autopp <autopp.inc@gmail.com>
2021-10-03 21:28:34 +0900

    feat: print fmt.Printf/fmt.Println

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"go/ast"
 	"go/parser"
+	"go/printer"
 	"go/token"
 	"log"
 	"os"
 )
+
+type visitor struct {
+	fset *token.FileSet
+}
+
+func (v *visitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return v
+	}
+	if c, ok := node.(*ast.CallExpr); ok {
+		if s, ok := c.Fun.(*ast.SelectorExpr); ok {
+			if p, ok := s.X.(*ast.Ident); ok {
+				if p.Name == "fmt" && (s.Sel.Name == "Printf" || s.Sel.Name == "Println") {
+					buf := &bytes.Buffer{}
+					printer.Fprint(buf, v.fset, node)
+					fmt.Printf("%s\n", buf.String())
+				}
+			}
+		}
+	}
+	return v
+}
 
 func main() {
 	fset := token.NewFileSet()
@@ -15,5 +40,6 @@ func main() {
 		log.Fatalf("ParseFile failed: %s", err)
 	}
 
-	fmt.Printf("%#v", f)
+	v := &visitor{fset}
+	ast.Walk(v, f)
 }
